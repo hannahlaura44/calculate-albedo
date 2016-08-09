02e10c5970a2e91e119136e77dd81a200103c5ab
Hannah Hagen <hannahlaura4@gmail.com>
2016-08-09 14:51:37 -0700

    save an image
    wrote a function that takes in a query and saves an image to your
    current directory of the image that is returned by that query. Also
    realized I have been switching the lat and lon and that explained why I
    was not able to find some images. I retrieved an image of UCB campus
    taken last week.

--- MODIS-notes.py
+++ MODIS-notes.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # MODIS
 # images the entire Earth every 1 to 2 days
 # wikipedia: https://en.wikipedia.org/wiki/Moderate-resolution_imaging_spectroradiometer
@@ -7,25 +9,96 @@
 # API key: gzvu4msw4Lc4nCh3CJtFU5bZqPdN5MyE0KM3HVtt
 # https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=gzvu4msw4Lc4nCh3CJtFU5bZqPdN5MyE0KM3HVtt
 # MODIS albedo product only has resolution of 500m (not good enough for 1 acre which is 63m x 63m)
+# resolution: Providing moderate-resolution imagery, from 15 meters to 100 meters, of Earth's land surface and polar regions, Landsat 8 operates in the visible, near-infrared, short wave infrared, and thermal infrared spectrum
+	# OLI multispectral bands 1-7,9: 30-meters
+	# OLI panchromatic band 8: 15-meters
+	# TIRS bands 10-11: collected at 100 meters but resampled
+	# to 30 meters to match OLI multispectral bands
+# they are updated frequently, I obtained images of UCB campus taken as recently as July 29. They seem to be uploaded in real time.
 
-import requests
-r = requests.get("https://api.nasa.gov/planetary/earth/imagery?lon=38.0&lat=40.0&date=2014-10-01&cloud_score=True&api_key=gzvu4msw4Lc4nCh3CJtFU5bZqPdN5MyE0KM3HVtt")
-#print "status code: ", r.status_code
-#print "headers: ", r.headers
+
+
+# import requests
+# r = requests.get("https://api.nasa.gov/planetary/earth/imagery?lon=38.0&lat=-120.0&date=2014-10-30&cloud_score=True&api_key=gzvu4msw4Lc4nCh3CJtFU5bZqPdN5MyE0KM3HVtt")
+# print "status code: ", r.status_code
+# print "headers: ", r.headers
 # print "content: ", r.content
 
+
+
+# function to save an image in the current directory given a request query QUERY and a filename FILENAME
+import requests
 from PIL import Image
 from StringIO import StringIO
-
-# s = StringIO(r.content)
-# img = Image.open(s)
-# img.save('pic','PNG')
-# open("foo.jpg", "w").write(img)
-
 import urllib, cStringIO
+def saveImage(query, filename):
+	print "accessing", query, "..."
+	r = requests.get(query)
+	imgURL = getImageURL(r)
+	if imgURL:
+		file = cStringIO.StringIO(urllib.urlopen(imgURL).read())
+		img = Image.open(file)
+		img.save(filename,'PNG')
+		print "image successfully saved in current directory as ", filename
+	if not imgURL:
+		print "error: did not find requested image."
+	# open("foo.jpg", "w").write(img)
 
-file = cStringIO.StringIO(urllib.urlopen("https://earthengine.googleapis.com/api/thumb?thumbid=283832cff020a0a5f527221e359847fe&token=de36c5c4d067147f79ddeba6ef6e7075").read())
+# function to get image url from Response contents
+def getImageURL(r):
+	i = 0
+	j = 0
+	foundurl = False
+	while i < len(r.content):
+		# beginning of url
+		if r.content[i:i+3] == "url":
+			i = i + 7 # move forward past these characters --> url": "
+			j = i
+			while j < len(r.content):
+				if r.content[j] == '"': # end of url
+					foundurl = True
+					break
+				j=j+1
+		if foundurl:
+			print "information about the image: ", r.content
+			return r.content[i:j]
+			break
+		i = i+1
+	if  not foundurl:
+		print "error: ", r.content
+	return
+# print "imageURL: ", getImageURL(r)
+
+# Tests
+def test1(): 
+	query = "https://api.nasa.gov/planetary/earth/imagery?lon=38.0&lat=40.0&date=2014-10-30&cloud_score=True&api_key=gzvu4msw4Lc4nCh3CJtFU5bZqPdN5MyE0KM3HVtt"
+	saveImage(query,"pic")
+def test2(): 
+	query = "https://api.nasa.gov/planetary/earth/imagery?lon=38.0&lat=-10.0&date=2014-10-30&cloud_score=True&api_key=gzvu4msw4Lc4nCh3CJtFU5bZqPdN5MyE0KM3HVtt"
+	saveImage(query,"pic2")
+def test3(): # UCB campus
+	query = "https://api.nasa.gov/planetary/earth/imagery?lon=-122.257813&lat=37.872003&date=2016-08-9&cloud_score=True&api_key=gzvu4msw4Lc4nCh3CJtFU5bZqPdN5MyE0KM3HVtt"
+	saveImage(query,"pic3")
+
+def test4(): 
+	query = "https://api.nasa.gov/planetary/earth/imagery?lon=38.0&lat=-120.0&date=2014-10-30&cloud_score=True&api_key=gzvu4msw4Lc4nCh3CJtFU5bZqPdN5MyE0KM3HVtt"
+	saveImage(query,"pic4")
+
+def test5(): 
+	query = "https://api.nasa.gov/planetary/earth/imagery?lon=38.0&lat=-120.0&date=2014-10-30&cloud_score=True&api_key=gzvu4msw4Lc4nCh3CJtFU5bZqPdN5MyE0KM3HVtt"
+	saveImage(query,"pic5")
+
+# main
+# test1()
+# test2()
+test3()
+# test4()
+# test5()
+
+
+
+# Now I should define a function to take in an image object and estimate the albedo.
+
 img = Image.open(file)
-img.save('pic','PNG')
 
 
